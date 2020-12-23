6e676ab08f2d5b8ea755f6a5e741c1db03d9bc88
Johnathan Maytliss <jmpstillsart@gmail.com>
2020-12-23 16:37:19 +0200

    Changing parse_uin_from_url to use regex

--- data_parser.py
+++ data_parser.py
@@ -1,4 +1,5 @@
 import logging
+import re
 from re import sub
 from decimal import Decimal
 import consts
@@ -33,22 +34,34 @@ def change_price_from_str_to_decimal(item_price):
     """Function that converting string of price to decimal."""
     decimal_price = Decimal(sub(r'[^\d.]', '', item_price))
     logger.debug(f'The price is {item_price}.')
+
     return decimal_price
 
 
 def parse_uin_from_url(url):
-    """Getting uin instead of the entire url"""
-    uin = ''
-    i = url.find('uin=')
-    if i < 0:
-        logger.error(consts.UIN_ERROR_MESSAGE)
-        raise Exception(consts.UIN_ERROR_MESSAGE)
-    else:
-        for i in range(i + 4, len(url)):
-            if url[i].isnumeric() and i != len(url):
-                uin += url[i]
-            else:
-                logger.debug(f'The uin is:{uin}.')
-                return uin
-        logger.debug(f'The uin is:{uin}.')
-        return uin
+    """Checking for 'uin=' and returning the number after it."""
+    regex = 'uin=(\d+)'
+    match = re.search(regex, url)
+    if match:
+        return match.group()[4:]
+
+    return False
+
+
+# parse_uin_from_url old function
+# def parse_uin_from_url(url):
+#     """Getting uin instead of the entire url"""
+#     uin = ''
+#     i = url.find('uin=')
+#     if i < 0:
+#         logger.error(consts.UIN_ERROR_MESSAGE)
+#         raise Exception(consts.UIN_ERROR_MESSAGE)
+#     else:
+#         for i in range(i + 4, len(url)):
+#             if url[i].isnumeric() and i != len(url):
+#                 uin += url[i]
+#             else:
+#                 logger.debug(f'The uin is:{uin}.')
+#                 return uin
+#         logger.debug(f'The uin is:{uin}.')
+#         return uin
